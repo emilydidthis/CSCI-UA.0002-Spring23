0a0f0eaa57d67141186b0039acc317f81fea0c85
Emily Zhao <emilyzhao@nyu.edu>
2023-02-06 15:30:29 -0500

    fixing code and slides

--- assets/code/guessing.py
+++ assets/code/guessing.py
@@ -5,7 +5,7 @@ secret = 13
 guess = int(input("Guess a number: "))
 
 # how do I check if a number is divisible by 7? modulo!
-if guess % 7 == 0 or guess == secret:
+if guess % 7 == 0 or secret:
     print("You win!")
 else:
     print("You lose!")
